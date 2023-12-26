2538efb526634cb658072e39cb939c6aacfa6d71
宋厚锦 <songhoujin@mediinfo.com.cn>
2023-12-26 13:11:55 +0800

    在CMake官网下载 关于 培训教程  推进到第二章

---
+++ CMakeProject01/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,19 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+#include <fstream>
+
+using namespace std;
+void Func();  //Func 此处声明
+int main()
+{
+	Func();// Func 此处调用
+	std::ofstream file;
+	file.open("tessssssssss.txt");
+	file << "HelloWord.\n";
+	file.close();
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
